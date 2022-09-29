7d4a0d34d660020faf10891446db5ba50afb6025
Hariharapranav <hpranav66@gmail.com>
2022-09-29 21:53:57 +0530

    First Commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+pannellum.viewer('panorama-360-view1', {
+    "type": "equirectangular",
+    "panorama": "https://i.ibb.co/4YNZBVF/puydesancy.jpg",
+    "autoLoad": true
+})
+
+pannellum.viewer('panorama-360-view2', {
+    "type": "equirectangular",
+    "panorama": "https://i.ibb.co/HhdXm4H/nature.jpg",
+    "autoLoad": true
+})
+
+pannellum.viewer('panorama-360-view3', {
+    "type": "equirectangular",
+    "panorama": "https://i.ibb.co/1b47Zfv/italy.jpg",
+    "autoLoad": true
+})
+
+pannellum.viewer('panorama-360-view4', {
+    "type": "equirectangular",
+    "panorama": "https://i.ibb.co/XjK2n8M/german.jpg",
+    "autoLoad": true
+})
+
+pannellum.viewer('panorama-360-view5', {
+    "type": "equirectangular",
+    "panorama": "https://i.ibb.co/w4dPWT8/island.jpg",
+    "autoLoad": true
+})
+
+pannellum.viewer('panorama-360-view6', {
+    "type": "equirectangular",
+    "panorama": "https://i.ibb.co/6NZpCDK/cambodia.jpg",
+    "autoLoad": true
+})
